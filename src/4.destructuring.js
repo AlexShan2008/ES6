958c6ab9852afaaaedaf6f9819e872caa8e0f8db
AlexShan <happyshantong@163.com>
2018-02-10 11:22:25 +0800

    destructuring

--- src/4.destructuring.js
+++ src/4.destructuring.js
@@ -5,13 +5,19 @@
  * 解构赋值；
  * 1、数组；
  * 2、对象；
- * 3、省去了遍历的过程；
+ * 3、字符串；
+ * 4、数值和布尔值；
+ * 5、函数参数；
+ * 6、Map
+ * 7、省去了遍历的过程；
+ * 只要不是数组和对象，都会先将右边的值转换为对象，因为undefined和null不能转为对象，所有报错。
  * 要求就是前后对应，名称一定要相同。
  * @type {[*]}
  */
 
 let arr = ['dog', 'cat', 'bull'];
 
+// 1. Array
 let [dog, , bull] = arr;
 
 console.log(dog); //dog;
@@ -19,25 +25,108 @@ console.log(bull);//bull
 
 
 let obj = {
-    name : "alexshan",
-    age : "28",
-    fn(){
+    name: "alexshan",
+    age: "28",
+    fn() {
         console.log(name)
     }
 };
 
-let {name,age} = obj;
+// 2.Object 
+let { name, age } = obj;
 
 console.log(name);//tony
 console.log(age);//28
 
-let {fn} =obj;
+let { fn } = obj;
 console.log(fn);
 //function fn() {
 //console.log(this.name);
 //}
 
-let {React,Component,ProtoType } = require('react');
+// 3.String
+const [a, b, c, d, e] = "hello";
+console.log(b); //e
+let { length: len } = "hello";
+console.log(len); //5
+
+// 4.数组和布尔值
+let {toString: s } =123;
+console.log(s);//123
+
+function* fibs() {
+    let a = 0;
+    let b = 1;
+    while (true) {
+        yield a;
+        [a, b] = [b, a + b];
+    }
+}
+
+let [first, second, third, forth, five, six] = fibs();
+
+console.log(six); //5 
+
+// 允许使用默认值
+let [x, foo = true] = [];
+console.log(foo);
+
+let { x = 3 } = {};
+console.log(x); //3
 
 
+// 对象的解构赋值不像数组按次序排列的，而是变量必须与属性同名才能取得正确的值。
+// 是下面形式的简写
 
+let { foo: foo, bar: bar } = { foo: "aaa", bar: "sex" }
+
+// 5.函数参数;
+function add([x,y]){
+    return x+y;
+}
+add([1,2]) //3
+
+
+/**
+ * 
+ * 解构赋值的用途:
+ * 1.交换变量的值
+ * 2.从函数返回多个值：函数只能返回1个值，但可以返回数组
+ * 3.函数参数的定义
+ * 4.提前JSON数据
+ * 5.函数参数的默认值
+ * 6.遍历Map结构
+ * 7.输入模块的指定方法
+ */
+
+
+//  6.遍历Map  for …… of循环遍历
+// for in遍历的是数组的索引（即键名），而for of遍历的是数组元素值。
+
+let map = new Map();
+map.set("first","hello");
+map.set("second","world");
+
+// 获取key和value
+for(let [key,value] of map){
+    console.log(key+"is"+value);
+}
+
+// 获取key
+for(let [key] of map){
+    console.log(key);
+}
+
+// 获取value
+for(let [,value] of map){
+    console.log(value);
+}
+
+
+// for in 循环
+for(let props in map){
+    console.log(props);
+}
+
+// 7.输入模块的指定方法
+const { React, Component, ProtoType } = require('react');
--- src/6.letConst.js
+++ src/6.letConst.js
@@ -2,9 +2,32 @@
  * Created by ShanGuo on 2017/6/23.
  */
 /**
+ * 共6种变量声明的方法：
+ * var 
+ * function 
+ * let 
+ * const 
+ * important 
+ * class
+ * 
  * let 局部变量；
- * const 常量；
+ * const 常量；声明一个只读的常量，一旦声明变量，指向的内存地址不变，就是指针不变。
+ * 1.简单类型（Number String Boolean）地址不变，值就不会变
+ * 2.复合型（Array Object),地址不变，但是对象本身可以改变，仍然可以添加新属性。
+ * 3.如果想对象不可变，冻结对象Object.freeze(obj)
  * 阻止了变量提升；
+ * 暂时性死区（TDZ) temple dead zone
+ * 
+ * 与传统var 声明的区别：
+ * 1.先声明后使用 
+ * 2.不可重复声明   let x = x //ReferenceError: x is not defined
+ * 3.块级作用域：是一个语句，没有返回结果，
+ * let  x = do{
+ *   let t = f();
+ *   t*t +1
+ * }
+ * 4.不存在变量提升
+ * 5.避免在块级作用域声明函数，必须声明的情况应该用函数表达式
  */
 
 // for (let i = 0; i < 10; i++) {
